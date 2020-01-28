df7d7d5dbf6a4faad98410288fd7a65f04ea2c2b
rstatz <robinstatz@gmail.com>
2020-01-28 11:13:26 -0700

    add service test file and import chai and expect

---
+++ email.service.test.js
@@ -0,0 +1,6 @@
+const chai = require('chai');
+const expect = chai.expect;
+const queryString = require('querystring');
+const emailService = require('./email.service.js');
+const emailCtrl = require('./email.controller');
+
